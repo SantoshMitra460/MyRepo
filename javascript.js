c7afc779fcbdad2616965d33ab5f37600b4a0cce
SantoshMitra460 <126327063+SantoshMitra460@users.noreply.github.com>
2023-02-25 17:58:19 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+console.log("Hello World")
+let a = 50;
+let b = 70;
+let c = a+b;
+console.log("The result of"+" "+a+" "+"and"+" "+b+" "+"is"+" "+c);
+const pi = 3.14
+var r = window.prompt("Enter the value of the radius of the circle");
+var circumference = 2*pi*r;
+console.log("The circumference of the circle is"+" "+circumference);
+let f = [8,9,90,67,34]
+let g = f.length;
+console.log("The length of the array is"+" "+g);
+console.log("The first element in the array is"+" "+f[0]);
+let h = f.push("Santosh");
+console.log(h);
+
+
+
